606b3982873323245a75255b9affc85eee7d2b06
kristinayax <kristinayax@icloud.com>
2023-06-14 12:27:29 +0300

    added test

---
+++ src/test/java/ru/netology/service/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnRequest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hello, netology!") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello, netology!"))
+        ;
+    }
+}
